6ff6575e8e27cd99a1dde1340718c97670fd9388
clarissajesselyn <clarissajesselyn7@gmail.com>
2025-02-13 13:39:29 +0700

    Update Case2: Library

--- Case2_Library/src/Main.java
+++ Case2_Library/src/Main.java
@@ -26,97 +26,93 @@ public class Main {
         
         library.displayLibraryBooks();
     }
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         new Main();
     }
+}
+class Author{
+    String name;
+    String biography;
 
-    public class Author{
-        String name;
-        String biography;
+    public Author(String name, String biography){
+        this.name = name;
+        this.biography = biography;
+    }
+}
 
-        public Author(String name, String biography){
-            this.name = name;
-            this.biography = biography;
-        }
-
+class Book{
+    String title;
+    String isbn;
+    Author author;
+    public Book(String title, String isbn, Author author){
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
     }
 
-    public class Book{
-        String title;
-        String isbn;
-        Author author;
-        public Book(String title, String isbn, Author author){
-            this.title = title;
-            this.isbn = isbn;
-            this.author = author;
+    public void displayBook(){
+        System.out.println("Title: " + title);
+        System.out.println("ISBN: " + isbn);
+        System.out.println("Author: " + author.name);
+        System.out.println("Biography: " + author.biography);
+        System.out.println("------------------------");
+    }
+}
+
+
+class Library{
+    ArrayList<Book> libraryBook = new ArrayList<Book>();
+    public void addBook(Book book){
+        System.out.println("Book added to library: " + book.title);
+        libraryBook.add (book);
+    }
+    public void borrowBook(Borrower borrower, Book book){
+        if (libraryBook.contains(book)){
+            System.out.println(borrower.name + " borrowed: " + book.title);
+            borrower.borrowBook(book);
+            libraryBook.remove(book);
+        } else {
+            System.out.println("The Book Not Found!!");
         }
     }
-
-    public class Library{
-        ArrayList<Book> libraryBook = new ArrayList<Book>();
-        public void addBook(Book book){
-            System.out.println("Book added to library: " + book.title);
+    public void returnBook(Borrower borrower, Book book){
+        if (borrower.returnBook(book)){
+            System.out.println(borrower.name + " returned: " + book.title);
             libraryBook.add (book);
-        }
-        public void borrowBook(Borrower borrower, Book book){
-            if (libraryBook.contains(book)){
-                System.out.println(borrower.name + " borrowed: " + book.title);
-                borrower.borrowBook(book);
-                libraryBook.remove(book);
-            } else {
-                System.out.println("The Book Not Found!!");
-            }
-        }
-        public void returnBook(Borrower borrower, Book book){
-            if (borrower.returnBook(book)){
-                System.out.println(borrower.name + " returned: " + book.title);
-                libraryBook.add (book);
-            } else {
-                System.out.println("Borrower has no such book!");
-            }
-        }
-        public void displayLibraryBooks(){
-            System.out.println("Books in Library:");
-            for (Book libBook:libraryBook){
-                System.out.println("Title: " + libBook.title);
-                System.out.println("ISBN: " + libBook.isbn);
-                System.out.println("Author: " + libBook.author.name);
-                System.out.println("Biography: " + libBook.author.biography);
-                System.out.println("------------------------");
-            }
-            for(Book libBook:libraryBook){
-                
-            }
+        } else {
+            System.out.println("Borrower has no such book!");
         }
     }
-
-    public class Borrower{
-        String name;
-        ArrayList<Book> borrowBooks = new ArrayList<Book>();
-        public Borrower(String name){
-            this.name = name;
+    public void displayLibraryBooks(){
+        System.out.println("Books in Library:");
+        for (Book libBook:libraryBook){
+            libBook.displayBook();
         }
+    }
+}
 
-        public void borrowBook(Book book){
-            borrowBooks.add(book);
-        }
-        public boolean returnBook(Book book){
-            if (borrowBooks.contains(book)){
-                borrowBooks.remove(book);
-                return true;
-            }
-            return false;
-        }
+class Borrower{
+    String name;
+    ArrayList<Book> borrowBooks = new ArrayList<Book>();
+    public Borrower(String name){
+        this.name = name;
+    }
 
-        public void displayBorrowedBooks(){
-            System.out.println(this.name+"'s Borrowed Books:");
-            for (Book borrowedBook:borrowBooks){
-                System.out.println("Title: " + borrowedBook.title);
-                System.out.println("ISBN: " + borrowedBook.isbn);
-                System.out.println("Author: " + borrowedBook.author.name);
-                System.out.println("Biography: " + borrowedBook.author.biography);
-                System.out.println("------------------------");
-            }
+    public void borrowBook(Book book){
+        borrowBooks.add(book);
+    }
+    public boolean returnBook(Book book){
+        if (borrowBooks.contains(book)){
+            borrowBooks.remove(book);
+            return true;
+        }
+        return false;
+    }
+
+    public void displayBorrowedBooks(){
+        System.out.println(this.name+"'s Borrowed Books:");
+        for (Book borrowedBook:borrowBooks){
+            borrowedBook.displayBook();
         }
     }
 }
