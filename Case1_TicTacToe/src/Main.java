f412cecf8d7b8f762c4304af6eace03f069d11a2
clarissajesselyn <clarissajesselyn7@gmail.com>
2025-02-18 10:34:58 +0700

    Update Tic Tac Toe Code

--- Case1_TicTacToe/src/Main.java
+++ Case1_TicTacToe/src/Main.java
@@ -1,80 +1,112 @@
 import java.util.*;
 
-public class Main {
-    public Scanner sc;
-    char [][] board = new char[3][3];
-    char player = 'O';
-    int numPlayer = 1;
+class Player{
+    public char play;
+    public int numPlayer;
+    public Player (char play, int numPlayer){
+        this.play = play;
+        this.numPlayer = numPlayer;
+    }
+}
 
-    public Main(){
-        sc = new Scanner (System.in);
-        System.out.println("Tic Tac Toe Game");
-
-        System.out.println("Board :");
+class Board{
+    public char [][] board = new char[3][3];
+    public Board(){
         for (int i = 0; i<3; i++){
             for (int j = 0; j<3; j++){
                 board[i][j] = '-';
-                System.out.print(board[i][j]);
             }
-            System.out.println("");
         }
-
-        int x, y;
-        int step = 0;
-        do {
-            // Input Coordinat to Play
-            do {
-                System.out.print("Player " + numPlayer + " move " + "(" + player + "): ");
-                x = sc.nextInt();
-                y = sc.nextInt();
-            } while (x < 0 || x > 2 || y < 0 || y > 2 || board[y][x]!='-');
-            board[y][x] = player;
-            printBoard();
-            step++;
-
-            // Check Winner
-            if (checkWinner(player)){
-                System.out.println("Player " + player +" Win!!");
-                break;
-            } else if (step == 9){
-                System.out.println("It's Draw!!");
-                break;
-            }
-
-            // Switch Player
-            if (player == 'O'){
-                player = 'X';
-                numPlayer = 2;
-            } else {
-                player = 'O';
-                numPlayer = 1;
-            }
-        } while (true);
     }
-
     public void printBoard(){
         for (int i = 0; i<3; i++){
             for (int j = 0; j<3; j++){
                 System.out.print(board[i][j]);
             }
-            System.out.println("");
+            System.out.println();
         }
     }
+    public void setBoard(int x, int y, char play){
+        board[y][x] = play;
+    }
+    public boolean checkBoard(int x, int y, char check){
+        if (board[y][x] == check){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class Game{
+    public Scanner sc;
+    public Board board;
+    public int step = 0;
+    
+    public Game(Board board){
+        this.board = board;
+    }
+    public boolean Playing(Player player){
+        sc = new Scanner (System.in);
+        int x, y;
+
+        do {
+            System.out.print("Player " + player.numPlayer + " move " + "(" + player.play + "): ");
+            x = sc.nextInt();
+            y = sc.nextInt();
+        } while (x < 0 || x > 2 || y < 0 || y > 2 || !board.checkBoard(x, y, '-'));
+        board.setBoard(x, y, player.play);
+        board.printBoard();
+        step++;
+        
+        if (checkWinner(player.play)){
+            System.out.println ("Player " + player.numPlayer + " (" + player.play + ") " + "WIN!!");
+            return true;
+        } else if (step == 9){
+            System.out.println ("It's Draw!!");
+            return true;
+        }
+        return false;
+    }
+
     public boolean checkWinner(char play){
         for (int i = 0; i<3; i++){
             // Horizontal & Vertical
-            if ((board[i][0] == play && board[i][1] == play && board[i][2] == play) || (board[0][i] == play && board[1][i] == play && board[2][i] == play)) {
+            if ((board.checkBoard(i, 0, play) && board.checkBoard(i, 1, play) && board.checkBoard(i, 2, play)) 
+             || (board.checkBoard(0, i, play) && board.checkBoard(1, i, play) && board.checkBoard(2, i, play))) {
                 return true;
             }
         }
-
+    
         // Diagonal
-        if ((board[0][0] == play && board[1][1] == play && board[2][2] == play) || (board[2][0] == play && board[1][1] == play && board[0][2] == play)){
+        if ((board.checkBoard(0, 0, play) && board.checkBoard(1, 1, play) && board.checkBoard(2, 2, play))
+         || (board.checkBoard(2, 0, play) && board.checkBoard(1, 1, play) && board.checkBoard(0, 2, play))){
             return true;
         }
-
+    
         return false;
     }
+}
+
+public class Main {
+    public Main(){
+        Player player1 = new Player ('O', 1);
+        Player player2 = new Player ('X', 2);
+
+        Board board = new Board();
+        
+        System.out.println("Tic Tac Toe Game");
+        board.printBoard();
+        
+        Game game = new Game(board);
+       
+        do {
+            if (game.Playing(player1)) break;
+            else if (game.Playing(player2)) break;
+        } while (true);
+    }
+
+    
 
     public static void main(String[] args) throws Exception {
         new Main();
